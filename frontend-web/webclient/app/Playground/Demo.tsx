ff69d9736f7e9fece495525efb67f0d17e9561f9
Jonas Malte Hinchely <jonas@hinchely.dk>
2021-05-17 09:45:35 +0200

    Attempt at resource allocation

--- frontend-web/webclient/app/Playground/Demo.tsx
+++ frontend-web/webclient/app/Playground/Demo.tsx
@@ -1,41 +1,154 @@
-import * as React from "react";
-import * as Heading from "ui-components/Heading";
-import {useState} from "react";
 import MainContainer from "MainContainer/MainContainer";
-import {ListRow} from "ui-components/List";
-import List from "ui-components/List";
-import {ImagePlaceholder} from "UtilityComponents";
-import {Button, ExternalLink} from "ui-components";
+import * as React from "react";
+import styled from "styled-components";
+import {Button, Card, Flex, Icon, Input} from "ui-components";
+import {Spacer} from "ui-components/Spacer";
+import Table, {TableCell, TableHeader, TableHeaderCell, TableRow} from "ui-components/Table";
+
+enum Selection {
+    ACTIVE,
+    ARCHIVED,
+    ALL
+}
+
+const mockData = [
+
+]
 
 const Demo: React.FunctionComponent = () => {
-    const [page, setPage] = useState(0);
+    const [selection, setSelection] = React.useState(Selection.ACTIVE);
+    return (
+        <MainContainer
+            main={<>
 
-    switch (page) {
-        case 0:
-        case 1:
-            return <MainContainer
-                main={
-                    <List>
-                        {["A", "B", "C", "D", "E", "F", "G"].map(it => {
-                            const connected = page == 1 && it === "A";
-                            return <ListRow
-                                key={it}
-                                icon={<img src={"https://placekitten.com/48/48"} />}
-                                left={<>Provider {it}</>}
-                                right={
-                                    connected ? <Button disabled>Connected</Button> :
-                                    <ExternalLink href={"https://deic-adm.sdu.dk/admin"} onClick={() => setPage(1)}>
-                                        <Button>Connect</Button>
-                                    </ExternalLink>
-                                }
-                            />;
-                        })}
-                    </List>
-                }
-            />;
-        default:
-            return null;
-    }
+                <Spacer
+                    left={null}
+                    right={
+                        <Flex height="42px">
+                            <Input width="150px" />
+                            <Button width="350px" ml="8px">Apply for more resources</Button>
+                            <Button ml="8px" width="230px">New subproject</Button>
+                        </Flex>
+                    }
+                />
+                <BladedCard>
+                    <div>
+                        <span onClick={() => setSelection(Selection.ACTIVE)} data-active={selection === Selection.ACTIVE}>Active</span>
+                        <span onClick={() => setSelection(Selection.ARCHIVED)} data-active={selection === Selection.ARCHIVED}>Archived</span>
+                        <span onClick={() => setSelection(Selection.ALL)} data-active={selection === Selection.ALL}>All</span>
+                        <div />
+                    </div>
+                    <Card>
+
+                        <Table>
+
+                            <TableHeader>
+
+                                <TableRow>
+                                    <TableHeaderCell textAlign="left">
+                                        Name
+                                    </TableHeaderCell>
+                                    <TableHeaderCell>
+                                        {/* Clickable dropdown? */}
+                                        <Icon name="cpu" color="black" /> Compute
+                                    </TableHeaderCell>
+                                    <TableHeaderCell>
+                                        {/* Clickable dropdown? */}
+                                        <Icon name="ftFileSystem" color="black" /> Storage
+                                    </TableHeaderCell>
+                                    <TableHeaderCell />
+                                </TableRow>
+                            </TableHeader>
+                            <tbody>
+                                <TableRow>
+                                    <TableCell textAlign="left">
+                                        Test
+                                    </TableCell>
+                                    <TableCell>
+                                        0 DKK (0.00% used)
+                                    </TableCell>
+                                    <TableCell>
+                                        0 DKK (0.00% used)
+                                    </TableCell>
+                                    <TableCell>
+                                        5.24 MB
+                                    </TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>
+                                        Work
+                                    </TableCell>
+                                    <TableCell>
+                                        0 DKK (0.00% used)
+                                    </TableCell>
+                                    <TableCell>
+                                        0 DKK (0.00% used)
+                                    </TableCell>
+                                    <TableCell>
+                                        5.24 MB
+                                    </TableCell>
+                                </TableRow>
+                            </tbody>
+                        </Table>
+                    </Card>
+                </BladedCard>
+            </>}
+        />
+    );
 };
+
+const BladedCard = styled.div`
+    /* BLADES  */
+    & > div:first-child {
+        background-color: var(--white);
+        display: flex;
+
+        & > span {
+            width: 90px;
+            padding-bottom: 2px;
+            margin-top: 8px;
+            cursor: pointer;
+            padding-left: 4px;
+            padding-right: 4px;
+            border-right: 0px;
+            border: solid 1px var(--midGray);
+            color: var(--midGray);
+            text-align: center;
+        }
+
+        & > span[data-active=true] {
+            border-bottom: 0px;
+            color: var(--blue);
+        }
+
+        & > span:last-child {
+            border-right: solid 1px var(--midGray);
+            margin-right: auto;
+        }
+
+        & > div:last-child {
+            width: 100%;
+            border-bottom: 1px solid var(--midGray);
+            background-color: unset;
+        }
+
+        & > div {
+            display: flex;
+        }
+    }
+
+    /* CARD */
+    & > ${Card} {
+        border-top: 0px;
+    }
+
+    & > ${Card} > table > thead > tr > th , & > ${Card} > table > tbody > tr > td {
+        padding-left: 8px;
+    }
+
+    & > ${Card} > table > thead > tr, & > ${Card} > table > tbody > tr {
+        height: 45px;
+    }
+`;
 
 export default Demo;
